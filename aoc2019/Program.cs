92f308dab0e74c0ff676c2313780c1f245ff3692
Tim Langens <tim.langens@mediahuis.be>
2019-12-02 09:46:42 +0100

    day1

---
+++ aoc2019/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace aoc2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Which day:");
+            var day = Console.ReadLine();
+            switch(day)
+            {
+                case "1":
+                    Day1.Execute();
+                    break;
+            };
+            Console.WriteLine("Waiting to quit...");
+            Console.ReadKey();
+        }
+    }
+}
