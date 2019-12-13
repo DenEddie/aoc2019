a35d1eded8111d2a74d982a59447915882d1a9df
Tim Langens <tim.langens@mediahuis.be>
2019-12-13 06:44:59 +0100

    Day12 - 13

--- aoc2019/Day12.cs
+++ aoc2019/Day12.cs
@@ -1,14 +1,167 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace aoc2019
 {
-   public class Day12
+    public class Day12
     {
-        public static void Execute() 
-		{ 
-            Console.WriteLine($"Not yet implemented");
-		}
+        public static void Execute()
+        {
+            Move(CopyList(MoonsTest1), 10);
+            Move(CopyList(MoonsTest2), 100);
+            Move(CopyList(Moons), 1000);
+            StepsBeforeMatchingState(MoonsTest1);
+            StepsBeforeMatchingState(MoonsTest2);
+            StepsBeforeMatchingState(Moons);
+        }
+
+        private static void Move(List<Coordinate> moons, long steps)
+        {
+            long step = 0;
+            List<Coordinate> velocities = moons.Select(m => new Coordinate(0, 0, 0)).ToList();
+            for (int i = 0; i < steps; i++)
+            {
+                CalculateVelocity(moons, velocities);
+                CalculatePosition(moons, velocities);
+            }
+            var totals = moons.Select((m, i) => (Math.Abs(m.X) + Math.Abs(m.Y) + Math.Abs(m.Z)) * (Math.Abs(velocities[i].X) + Math.Abs(velocities[i].Y) + Math.Abs(velocities[i].Z))).Sum();
+            Console.WriteLine($"After {steps} the sum of total energy is {totals}");
+        }
+
+        private static void StepsBeforeMatchingState(List<Coordinate> moons)
+        {
+           long xSteps = CalculateStepsInAxis(moons.Select(m => new Moon(m.X)).ToList());
+           long ySteps = CalculateStepsInAxis(moons.Select(m => new Moon(m.Y)).ToList());
+           long zSteps = CalculateStepsInAxis(moons.Select(m => new Moon(m.Z)).ToList());
+
+            Console.WriteLine($"Steps before matching: {LeastCommonMultiple(LeastCommonMultiple(xSteps, ySteps), zSteps)}");
+        }
+
+        private static int CalculateStepsInAxis(List<Moon> axisMoons)
+        {
+            var hashSet = new HashSet<string>();
+            var step = 0;
+            do
+            {
+                hashSet.Add(string.Concat(axisMoons));
+                step++;
+                foreach (var moon in axisMoons)
+                {
+                    foreach (var other in axisMoons)
+                    {
+                        if (moon != other)
+                        {
+                            moon.ProcessGravity(other);
+                        }
+                    }
+                }
+                foreach (var moon in axisMoons)
+                {
+                    moon.Move();
+                }
+            } while (hashSet.Count == step);
+
+            return hashSet.Count;
+        }
+
+        public static long LeastCommonMultiple(long a, long b)
+        {
+            return a * b / GreatestCommonDivider(a, b);
+        }
+
+        public static long GreatestCommonDivider(long a, long b)
+        {
+            if (b == 0)
+            {
+                return a;
+            }
+            else
+            {
+                return GreatestCommonDivider(b, a % b);
+            }
+        }
+
+        private static void CalculateVelocity(List<Coordinate> moons, List<Coordinate> velocities)
+        {
+            Parallel.For(0, velocities.Count, i =>
+             {
+                 velocities[i].X += moons.Sum(m => m.X > moons[i].X ? 1 : (moons[i].X == m.X ? 0 : -1));
+                 velocities[i].Y += moons.Sum(m => m.Y > moons[i].Y ? 1 : (moons[i].Y == m.Y ? 0 : -1));
+                 velocities[i].Z += moons.Sum(m => m.Z > moons[i].Z ? 1 : (moons[i].Z == m.Z ? 0 : -1));
+             });
+        }
+        private static void CalculatePosition(List<Coordinate> moons, List<Coordinate> velocities)
+        {
+            Parallel.For(0, velocities.Count, i =>
+            {
+                moons[i].X += velocities[i].X;
+                moons[i].Y += velocities[i].Y;
+                moons[i].Z += velocities[i].Z;
+            });
+        }
+
+        private static List<Coordinate> CopyList(List<Coordinate> list) => list.Select(c => new Coordinate(c.X, c.Y, c.Z)).ToList();
+
+        private static List<Coordinate> MoonsTest1 = new List<Coordinate>{
+                                                                    new Coordinate(-1,0,2),
+                                                                    new Coordinate(2,-10,-7),
+                                                                    new Coordinate(4,-8,8),
+                                                                    new Coordinate(3,5,-1)
+        };
+
+        private static List<Coordinate> MoonsTest2 = new List<Coordinate>{
+                                                                    new Coordinate(-8,-10,0),
+                                                                    new Coordinate(5,5,10),
+                                                                    new Coordinate(2,-7,3),
+                                                                    new Coordinate(9,-8,-3)
+        };
+        private static List<Coordinate> Moons = new List<Coordinate>{
+                                                                    new Coordinate(5,4,4),
+                                                                    new Coordinate(-11,-11,-3),
+                                                                    new Coordinate(0,7,0),
+                                                                    new Coordinate(-13,2,10),
+        };
+
+        private class Coordinate
+        {
+            public Coordinate(int x, int y, int z)
+            {
+                X = x;
+                Y = y;
+                Z = z;
+            }
+            public int X { get; set; }
+            public int Y { get; set; }
+            public int Z { get; set; }
+
+            public override string ToString() => $"({X},{Y},{Z})";
+        }
+
+        private class Moon
+        {
+            private int position;
+            private int velocity;
+
+            public Moon(int position)
+            {
+                this.position = position;
+            }
+
+            public void Move()
+            {
+                position += velocity;
+            }
+
+            public void ProcessGravity(Moon other)
+            {
+                if (this.position < other.position) velocity++;
+                if (this.position > other.position) velocity--;
+            }
+
+            public override string ToString() => $"({position},{velocity})";
+        }
     }
 }
---
+++ aoc2019/Migrations/20191212101016_InitialCreate.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace aoc2019.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SpaceState",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PositionX1 = table.Column<int>(nullable: false),
+                    PositionY1 = table.Column<int>(nullable: false),
+                    PositionZ1 = table.Column<int>(nullable: false),
+                    PositionX2 = table.Column<int>(nullable: false),
+                    PositionY2 = table.Column<int>(nullable: false),
+                    PositionZ2 = table.Column<int>(nullable: false),
+                    PositionX3 = table.Column<int>(nullable: false),
+                    PositionY3 = table.Column<int>(nullable: false),
+                    PositionZ3 = table.Column<int>(nullable: false),
+                    PositionX4 = table.Column<int>(nullable: false),
+                    PositionY4 = table.Column<int>(nullable: false),
+                    PositionZ4 = table.Column<int>(nullable: false),
+                    VelocityX1 = table.Column<int>(nullable: false),
+                    VelocityY1 = table.Column<int>(nullable: false),
+                    VelocityZ1 = table.Column<int>(nullable: false),
+                    VelocityX2 = table.Column<int>(nullable: false),
+                    VelocityY2 = table.Column<int>(nullable: false),
+                    VelocityZ2 = table.Column<int>(nullable: false),
+                    VelocityX3 = table.Column<int>(nullable: false),
+                    VelocityY3 = table.Column<int>(nullable: false),
+                    VelocityZ3 = table.Column<int>(nullable: false),
+                    VelocityX4 = table.Column<int>(nullable: false),
+                    VelocityY4 = table.Column<int>(nullable: false),
+                    VelocityZ4 = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SpaceState", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SpaceState_PositionX1_PositionX2_PositionX3_PositionX4_PositionY1_PositionY2_PositionY3_PositionY4_PositionZ1_PositionZ2_PositionZ3_PositionZ4_VelocityX1_VelocityX2_VelocityX3_VelocityX4_VelocityY1_VelocityY2_VelocityY3_VelocityY4_VelocityZ1_VelocityZ2_VelocityZ3_VelocityZ4",
+                table: "SpaceState",
+                columns: new[] { "PositionX1", "PositionX2", "PositionX3", "PositionX4", "PositionY1", "PositionY2", "PositionY3", "PositionY4", "PositionZ1", "PositionZ2", "PositionZ3", "PositionZ4", "VelocityX1", "VelocityX2", "VelocityX3", "VelocityX4", "VelocityY1", "VelocityY2", "VelocityY3", "VelocityY4", "VelocityZ1", "VelocityZ2", "VelocityZ3", "VelocityZ4" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SpaceState");
+        }
+    }
+}
